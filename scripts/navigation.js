440c09ddabd6747220216013c6e07da9d5496600
gitvuyelwa <vmacanda@byupathway.edu>
2025-07-07 19:59:24 +0200

    drive

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,6 +1,9 @@
 const navButton = document.querySelector('#ham-btn');
 const navLinks = document.querySelector('#nav-bar');
-
+const lastModified = document.lastModified;
+document.getElementById("lastModif").textContent = lastModified;
+const currentYear = document.currentYear;
+document.getElementById("currentyear").textContent = currentYear;
 
 navButton.addEventListener('click', () => {
   navButton.classList.toggle('show');
